cfb61af73237a796790dee162029eef4dd575af9
suzza <suzza@susansilver.net>
2025-01-21 11:06:09 -0800

    add index page elements

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "We Build Blogs";
+export const SITE_DESCRIPTION =
+  "Affordable web design for personal websites and blogs";
