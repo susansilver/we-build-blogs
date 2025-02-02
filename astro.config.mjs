0cfb2e9b043b6e87f19fe79c7c2ce9d188ba6546
suzza <suzza@susansilver.net>
2025-02-02 06:33:09 -0800

    add typography plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://webuildblogs.com",
-  integrations: [tailwind()],
+  integrations: [],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
